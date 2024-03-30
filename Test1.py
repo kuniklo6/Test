326eedf9ccf3ddd5a6ec77a988b2142ae0e1aced
Frank Conejo <kuniklo.vt@gmail.com>
2024-03-30 10:11:10 +0000

    Validation

--- Test1.py
+++ Test1.py
@@ -1,4 +1,20 @@
-print("Hello World")
-print("Hiya")
-var1 = 4
-print(var1)
+
+def hello(n, s):
+    for x in range(n):
+        print(x + 1, s)
+
+while True:
+    try:
+        num = int(input('Hoe many times? '))
+        break
+    except ValueError:
+        print('Please enter a whole number')
+
+# print('Your age is: ' + str(age))
+
+#Reference: https://www.easypythondocs.com/validation.html
+
+hello(int(num), "Hello")
+# print("Hiya")
+# var1 = 4
+# print(var1)
